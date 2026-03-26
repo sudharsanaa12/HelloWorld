4e8ae158d6c72cb02f307ac35c9642a971813f65
sudharsanaa12 <sa0683@srmist.edu.in>
2026-03-26 09:05:02 +0530

    uc3: added default handling for missing arguements

--- HelloApp/src/main/java/org/example/HelloApp.java
+++ HelloApp/src/main/java/org/example/HelloApp.java
@@ -1,13 +1,15 @@
 package org.example;
-
 public class HelloApp {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            System.out.println("Hello " +args[0]);
+        String name;
+        if(args.length > 0) {
+            name = args[0];
         }
         else {
-            System.out.println("Hello World");
+            name = "World";
         }
+
+        System.out.println("Hello " + name);
 
     }
 }
