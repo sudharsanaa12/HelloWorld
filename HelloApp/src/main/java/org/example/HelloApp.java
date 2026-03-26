e5a9dd37740d017e1a2e38dc0e32b28f3ac862f9
sudharsanaa12 <sa0683@srmist.edu.in>
2026-03-26 08:54:23 +0530

    UC2: Add command-line argument support

--- HelloApp/src/main/java/org/example/HelloApp.java
+++ HelloApp/src/main/java/org/example/HelloApp.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        if (args.length > 0) {
+            System.out.println("Hello " +args[0]);
+        }
+        else {
+            System.out.println("Hello World");
+        }
+
     }
 }
